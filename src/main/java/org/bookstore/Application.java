dea49e1061bfa6300014ada2e020c4f4252e544d
TomekGalazka <tom3k.galazka@gmail.com>
2024-02-07 23:09:46 +0100

    Create project structure

---
+++ src/main/java/org/bookstore/Application.java
@@ -0,0 +1,9 @@
+package org.bookstore;
+
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hear of the application - in development");
+    }
+}
