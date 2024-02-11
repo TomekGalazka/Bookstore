ea3aef9267d6a5ea45946c1c35bdc4cd7151994b
TomekGalazka <tom3k.galazka@gmail.com>
2024-02-11 17:02:45 +0100

    Refactor Bookstore class and Store interface, add Client class

--- src/main/java/org/bookstore/service/BookstoreService.java
+++ src/main/java/org/bookstore/service/BookstoreService.java
@@ -1,4 +1,52 @@
 package org.bookstore.service;
 
-public class BookstoreService {
+import org.bookstore.books.Book;
+import org.bookstore.clients.Client;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BookstoreService implements StoreService<Book, Long, Book, Client> {
+
+
+    @Override
+    public void addItem(Book item, Set<Book> itemSet) {
+
+    }
+
+    @Override
+    public void removeItem(Book itemType, Set<Book> itemSet) {
+
+    }
+
+    @Override
+    public void editItem(Book oldItem, Book newItem, Set<Book> itemSet) {
+
+    }
+
+    @Override
+    public void addItemCategory(Book itemCategory, Set<Book> itemCategorySet) {
+
+    }
+
+    @Override
+    public void reserveItem(Book item, Set<Book> itemSet, Client client, Map<Long, List<Book>> reservedItems) {
+
+    }
+
+    @Override
+    public void sellItem(Book item, Set<Book> itemSet, Client client, Map<Long, List<Book>> soldItems) {
+
+    }
+
+    @Override
+    public void generateReport(Set<Book> itemSet, Map<Long, Book> soldItems) {
+
+    }
+
+    @Override
+    public void addClient(Client client, Set<Client> clientSet) {
+
+    }
 }
--- src/main/java/org/bookstore/service/StoreService.java
+++ src/main/java/org/bookstore/service/StoreService.java
@@ -1,9 +1,10 @@
 package org.bookstore.service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public interface StoreService<T, K, V> {
+public interface StoreService<T, K, V, C> {
 
     void addItem(T item, Set<T> itemSet );
     void removeItem(T itemType, Set<T> itemSet);
@@ -11,10 +12,12 @@ public interface StoreService<T, K, V> {
 
     void addItemCategory(T itemCategory, Set<T> itemCategorySet);
 
-    void reserveItem (T item, Set<T> itemSet, Map<K, V> reservedItems);
+    void reserveItem (T item, Set<T> itemSet, C client, Map<K, List<V>> reservedItems);
 
-    void sellItem (T item, Set<T> itemSet, Map<K, V> soldItems);
+    void sellItem (T item, Set<T> itemSet, C client, Map<K, List<V>> soldItems);
 
     void generateReport(Set<T> itemSet, Map<K, V> soldItems);
+
+    void addClient(C client, Set<C> clientSet);
 
 }
